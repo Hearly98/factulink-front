36653eccf52510ce2dac6280e46c7ad8c75650fd
Hearly98 <shuertas981@gmail.com>
2026-02-02 03:59:58 +0100

    feat: cambiar api en cym-prpd

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://factulink-back.freekeybusiness.com/api/v1',
+  apiUrl: 'https://cympe-back.freekeybusiness.com/api/v1',
   igv: 1.18,
 };
